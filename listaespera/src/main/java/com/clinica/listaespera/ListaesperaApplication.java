b3fb3746a4690d3bb9ec359ed41805884232e5aa
NoSoyAle <al.cereno@duocuc.cl>
2026-04-17 18:52:58 -0400

    Inicializando proyecto RedNorte

---
+++ listaespera/src/main/java/com/clinica/listaespera/ListaesperaApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.listaespera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaesperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaesperaApplication.class, args);
+	}
+
+}
---
+++ listaespera/src/test/java/com/clinica/listaespera/ListaesperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.listaespera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaesperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
