e9cc69fff5b000ef2bc76fe67c98cbbcfedb897c
NoSoyAle <al.cereno@duocuc.cl>
2026-05-12 04:43:27 -0400

    Apigateway ffuncionando

--- apigateway/src/main/java/com/apireservas/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/apireservas/apigateway/ApigatewayApplication.java
@@ -1,4 +1,4 @@
-package com.clinica.apigateway;
+package com.apireservas.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
