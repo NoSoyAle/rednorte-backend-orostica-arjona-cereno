fb4c70be98dd947e39b15612d49cd15a91aaea36
AntoniaArjMor <an.arjona@duocuc.cl>
2026-06-12 20:22:03 -0400

    Se elimina Mcroservicio Citas y se mejora microservicio Agenda

--- agenda/src/main/java/com/clinica/agenda/entities/Doctor.java
+++ agenda/src/main/java/com/clinica/agenda/entities/Doctor.java
@@ -42,8 +42,6 @@ public class Doctor {
     @Enumerated(EnumType.STRING)
     private Sexo sexo;
 
-    @OneToMany(mappedBy = "doctor")
-    private List<BloqueHorario> bloques;
 
     @ManyToMany
     @JoinTable(
